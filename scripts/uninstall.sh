636a0ad0e6da44a1bd92ea96f2e0e60e98f391c5
Norviah <21983700+Norviah@users.noreply.github.com>
2024-05-28 11:17:18 -0700

    build(scripts): add a script for installing and uninstalling the mod

---
+++ scripts/uninstall.sh
@@ -0,0 +1,4 @@
+source "$(git rev-parse --show-toplevel)/scripts/paths.sh"
+
+rm -rf "${paths[CYBERPUNK_ROOT]}/${paths[CET_PATH]}/${paths[NAME]}"
+rm -rf "${paths[CYBERPUNK_ROOT]}/${paths[REDSCRIPT_PATH]}/${paths[NAME]}"
